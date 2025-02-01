830902128a17f70b234c760c0e807d870a79f513
tester9071348 <tester9071348@outlook.com>
2025-02-01 14:48:04 +0300

    Update firefoxpatch.c

--- 01-Extended DLLs/KxCom/firefoxpatch.c
+++ 01-Extended DLLs/KxCom/firefoxpatch.c
@@ -11,10 +11,7 @@ HRESULT WINAPI Ext_CoCreateInstance(
 	OUT	LPVOID		*ppv)
 {
 	unless (KexData->IfeoParameters.DisableAppSpecific){
-		if (AshExeBaseNameIs(L"firefox.exe") ||
-			AshExeBaseNameIs(L"thunderbird.exe") || 
-			AshExeBaseNameIs(L"betterbird.exe")) {
-			
+		if (AshExeBaseNameIs(L"firefox.exe")) {
 			if ((IsEqualCLSID(rclsid, &CLSID_DestinationList) && pUnkOuter == NULL && dwClsContext == CLSCTX_INPROC_SERVER && IsEqualIID(riid, &IID_ICustomDestinationList)) || (pUnkOuter == NULL && dwClsContext == CLSCTX_INPROC_SERVER && IsEqualIID(riid, &IID_IObjectCollection))) return E_NOTIMPL;
 		}
 	}
